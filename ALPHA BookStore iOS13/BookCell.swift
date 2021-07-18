fadbf01bb2dff06ea122f7bf0d827ace2efb07cd
PrynsTag <princevelasco16@gmail.com>
2021-07-18 17:44:17 +0800

    Added Table Cell custom design

---
+++ ALPHA BookStore iOS13/BookCell.swift
@@ -0,0 +1,23 @@
+//
+//  BookCell.swift
+//  ALPHA BookStore iOS13
+//
+//  Created by Prince Carl Velasco on 7/17/21.
+//
+
+import UIKit
+
+class BookCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
