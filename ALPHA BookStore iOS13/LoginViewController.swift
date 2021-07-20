de44e9fef279ffd883d54f12e92712db90d84019
PrynsTag <princevelasco16@gmail.com>
2021-07-21 07:40:25 +0800

    Added simple login

---
+++ ALPHA BookStore iOS13/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.swift
+//  ALPHA BookStore iOS13
+//
+//  Created by Prince Carl Velasco on 7/21/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func loginPressed(_ sender: Any) {
+        performSegue(withIdentifier: "goToStore", sender: self)
+    }
+}
