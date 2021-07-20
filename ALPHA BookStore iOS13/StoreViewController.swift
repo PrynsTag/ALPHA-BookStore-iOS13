d543a69cf7417266951d7f42375fe912f5d8599c
PrynsTag <princevelasco16@gmail.com>
2021-07-21 07:38:26 +0800

    Added multiple selection and deletion

--- ALPHA BookStore iOS13/StoreViewController.swift
+++ ALPHA BookStore iOS13/StoreViewController.swift
@@ -15,16 +15,19 @@ class StoreViewController: UIViewController {
     
     var books = [BookData]()
     var selectedBooks = [BookData]()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.delegate = self
-        self.tableView.dataSource = self
+        bookManager.delegate = self
+        
+        tableView.dataSource = self
+        bookManager.fetchBook()
+        
         tableView.register(UINib(nibName: "BookCell", bundle: nil), forCellReuseIdentifier: "ReusableCell")
         
-        bookManager.delegate = self
-        bookManager.fetchBook()
+        tableView.allowsMultipleSelection = true
     }
     
     @IBAction func checkoutButtonClicked(_ sender: Any) {
@@ -41,9 +44,9 @@ class StoreViewController: UIViewController {
 
 extension StoreViewController: BookManagerDelegate {
     func fetchBookData(_ bookManager: BookManager, bookData: [BookData]) {
-        DispatchQueue.main.async {
-            self.books.append(contentsOf: bookData[20..<40])
-            self.tableView.reloadData()
+        DispatchQueue.main.async { [self] in
+            books.append(contentsOf: bookData)
+            tableView.reloadData()
         }
     }
     
@@ -61,35 +64,36 @@ extension StoreViewController: UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ReusableCell", for: indexPath) as! BookCell
         
         DispatchQueue.main.async { [self] in
-            cell.title?.text = books[indexPath.row].title
-            
-            // Image
-            if let url = URL(string: (books[indexPath.row].thumbnailUrl) ?? "") {
+            if let url = URL(string: books[indexPath.row].thumbnailUrl!) {
                 let data = try? Data(contentsOf: url)
-                cell.bookImage.contentMode = .scaleToFill
                 cell.bookImage.image = UIImage(data: data!)
+                cell.bookImage.contentMode = .scaleToFill
                 
+                cell.title?.text = books[indexPath.row].title
                 cell.author?.text = books[indexPath.row].authors.joined(separator: "\n")
                 cell.page?.text = "\(books[indexPath.row].pageCount)"
                 cell.title?.text = books[indexPath.row].title
-                cell.price?.text = "\(Int.random(in: 100..<999))"
+                
+                books[indexPath.row].price = Int.random(in: 100..<999)
+                cell.price?.text = "\(books[indexPath.row].price!)"
             }
         }
-        
         return cell
     }
 }
 
 extension StoreViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
-        
-        if tableView.cellForRow(at: indexPath)?.isSelected != false {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            cell.accessoryType = .checkmark
             selectedBooks.append(books[indexPath.row])
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            cell.accessoryType = .none
+            selectedBooks = selectedBooks.filter { $0.title != books[indexPath.row].title }
         }
     }
 }
