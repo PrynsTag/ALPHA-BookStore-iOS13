ecae01090fab1d44b802bd2ad886a269c2b56c41
PrynsTag <princevelasco16@gmail.com>
2021-07-21 17:22:18 +0800

    Added modal for successfull transaction and dismiss functionality

--- ALPHA BookStore iOS13/PaymentModal.swift
+++ ALPHA BookStore iOS13/PaymentModal.swift
@@ -5,4 +5,13 @@
 //  Created by Prince Carl Velasco on 7/21/21.
 //
 
-import Foundation
+import UIKit
+
+class PaymentModal: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func myUnwindAction(unwindSegue: UIStoryboardSegue) {
+        dismiss (animated: true, completion: nil)
+    }
+}
